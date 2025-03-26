a8f6d73dca6d96f50f3d1129d4c8027bb471a2fa
shreekanth7899 <shreekanth7899@github.com>
2025-03-26 17:56:47 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3005;
+
+app.get("/", (req,res) => {
+    res.send("This is From JEnksins CI-CD using Docker");
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+});
